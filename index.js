b11c867412401437567ea55f447862d6c0771f36
Palashdada <palashnetworkgazipur@gmail.com>
2025-11-09 00:44:37 +0600

    mobgodb

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 3000;
+const uri =
+  "mongodb+srv://eco-track:6CDEXlO0HuSTzUuE@cluster0.hgrlmye.mongodb.net/?appName=Cluster0";
 
 app.get("/", (req, res) => {
   res.send("Server is ok");
@@ -9,3 +12,30 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log(` app listening on port ${port}`);
 });
+
+// eco-track
+
+// 6CDEXlO0HuSTzUuE
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+  }
+}
+run().catch(console.dir);
