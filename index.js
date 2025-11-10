9d5d8c2fb5a791d4ba03d7099ec243f2113378f0
Palashdada <palashnetworkgazipur@gmail.com>
2025-11-10 10:05:22 +0600

    liveStatics

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
+const cors = require("cors");
 const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
 const uri =
   "mongodb+srv://eco-track:6CDEXlO0HuSTzUuE@cluster0.hgrlmye.mongodb.net/?appName=Cluster0";
 
@@ -13,11 +18,6 @@ app.listen(port, () => {
   console.log(` app listening on port ${port}`);
 });
 
-// eco-track
-
-// 6CDEXlO0HuSTzUuE
-
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -29,6 +29,36 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    const db = client.db("eco-track-db");
+    const challengesColl = db.collection("challenges-Collection");
+    const tipsCall = db.collection("tips-Collection");
+    const eventsCall = db.collection("events-Collection");
+    app.get("/hero-challenges", async (req, res) => {
+      const result = await challengesColl
+        .find()
+        .sort({
+          participants: -1,
+        })
+        .limit(4)
+        .toArray();
+      res.send(result);
+    });
+    app.get("/live-statics", async (req, res) => {
+      const totalChallenges = await challengesColl.countDocuments();
+      const totalParticipants = await challengesColl
+        .aggregate([
+          { $group: { _id: null, total: { $sum: "$participants" } } },
+        ])
+        .toArray();
+      const totalTips = await tipsCall.countDocuments();
+      const totalEvents = await eventsCall.countDocuments();
+      res.send({
+        totalParticipants: totalParticipants[0].total || 0,
+        totalEvents,
+        totalTips,
+        totalChallenges,
+      });
+    });
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
